d40ec19f158bec0c17f170fcc56056f518fe5679
哆啦A梦 <460247986@qq.com>
2017-12-18 14:29:51 +0800

    创建工程
    Signed-off-by: 哆啦Aæ¢? <460247986@qq.com>

---
+++ src/Dapper.AutoMigrate/Dapper.AutoMigrate/Engine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper.AutoMigrate
+{
+    public class Engine
+    {
+        protected static string ConnectionString;
+        private static string ProviderInvariantName;
+
+        private static ConcurrentDictionary<Type, object> paramCache = new ConcurrentDictionary<Type, object>();
+
+        public static IDbConnection GetDbConnection()
+        {
+            DbProviderFactory dbProviderFactory = DbProviderFactories.GetFactory(ProviderInvariantName);
+            DbConnection dbConnection = dbProviderFactory.CreateConnection();
+            dbConnection.ConnectionString = ConnectionString;
+            return dbConnection;
+        }
+
+        public static void RegisterDataBase(string providerInvariantName, string connectionString)
+        {
+            Engine.ProviderInvariantName = providerInvariantName;
+            Engine.ConnectionString = connectionString;
+        }
+
+        public static void RegisterModel(Type type)
+        {
+            if (paramCache.ContainsKey(type))
+            {
+                throw new Exception("类型已经存在！");
+            }
+            paramCache.TryAdd(type, null);
+        }
+
+        public static void RunSync()
+        {
+
+        }
+    }
+}
